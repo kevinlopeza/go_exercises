330bb2ea8f7629eaae49315a2e8eb61c740dd3d4
Kevin <00251716@uca.edu.sv>
2021-03-29 17:06:57 -0600

    uploading exercise 3.10

---
+++ chap3/e3_10.go
@@ -0,0 +1,42 @@
+/* Write a non-recursive version of comma,  
+ * using bytes.Buffer instead of string concatenation
+ */
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for _, str := range os.Args[1:] {
+		fmt.Println(comma(str))
+	}
+}
+
+func comma(s string) string {
+	var buf bytes.Buffer
+	
+	n := len(s)
+
+	if n <= 3 {
+		return s
+	}
+
+	i := n % 3
+	
+	if i == 0 {
+		buf.WriteString(s[0:3])
+		i = 3
+	} else {
+		buf.WriteString(s[:i])
+	}
+
+	
+	for j := i; j < n; j = j + 3 {
+		buf.WriteString( "," + s[j:j+3])
+	}
+
+	return buf.String() 
+}
