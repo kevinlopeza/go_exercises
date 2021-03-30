be47aafd99c5257150132bd50511831d6adc580e
Kevin <00251716@uca.edu.sv>
2021-03-29 18:33:33 -0600

    Uploading exercise 3.12

--- chap3/e3_11.go
+++ chap3/e3_11.go
@@ -12,57 +12,10 @@ import (
 
 func main() {
 	for _, str := range os.Args[1:] {
-		fmt.Println(decimal(str))
+		fmt.Println(comma(str))
 	}
-}
-
-func decimal(s string) string {
-
-	sl := strings.Split(s, ".")
-
-	var sign string
-
-	if strings.HasPrefix(sl[0], "-") || strings.HasPrefix(sl[0], "+") {
-		sign = string(sl[0][0])
-	}
-
-	if len(sl) == 1 {
-		if sign == "" {
-			return comma(s)
-		} else {
-			return sign + comma(s[1:])
-		}
-	} else {
-		if sign == "" {
-			return comma(sl[0]) + "." + sl[1]
-		} else {
-			return sign + comma(sl[0][1:]) + "." + sl[1]
-		}
-	}
-
 }
 
 func comma(s string) string {
-	var buf bytes.Buffer
 
-	n := len(s)
-
-	if n <= 3 {
-		return s
-	}
-
-	i := n % 3
-
-	if i == 0 {
-		buf.WriteString(s[0:3])
-		i = 3
-	} else {
-		buf.WriteString(s[:i])
-	}
-
-	for ; i < n; i = i + 3 {
-		buf.WriteString("," + s[i:i+3])
-	}
-
-	return buf.String()
 }
